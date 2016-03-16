eb3d60f091bd080d7841c7e4d542a29b026f4be9
miguel-angulo-tierconnect-com <miguel.angulo@coderoad.com>
2016-03-15 21:59:26 -0430

    Delete Files.

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -4,7 +4,7 @@
     'triangular',
     'ngAnimate', 'ngCookies', 'ngSanitize', 'ngMessages', 'ngMaterial',
     'ui.router', 'pascalprecht.translate', 'LocalStorageModule', 'googlechart', 'chart.js', 'linkify', 'ui.calendar', 'angularMoment', 'textAngular', 'uiGmapgoogle-maps', 'hljs', 'md.data.table', angularDragula(angular), 'ngFileUpload',
-     'seed-module',
+     //'seed-module',
     // uncomment above to activate the example seed module
     'app.examples',
   ]).constant('APP_LANGUAGES', [{ // create a constant for languages so they can be added to both triangular & translate
